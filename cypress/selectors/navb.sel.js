94eb5299ec37308b7905e4a207f1716d6da0a6be
Andrea <andreavlas97@gmail.com>
2022-07-30 13:05:22 +0300

    tested hedear elements and links

---
+++ cypress/selectors/navb.sel.js
@@ -0,0 +1,22 @@
+export default {
+  productStoreLink: "a[class='navbar-brand'",
+  existingNavbLinks: "div[class='navbar-collapse'",
+  productStoreImage: "a[class='navbar-brand'",
+  homeLink: "a[class='nav-link'",
+  contactLink: "#navbarExample > ul > li:nth-child(2) > a",
+  contactModal: "#exampleModal > div > div",
+  closeContactModal:
+    " #exampleModal > div > div > div.modal-footer > button.btn.btn-secondary",
+  aboutUsLink: "#navbarExample > ul > li:nth-child(3) > a",
+  aboutUsModal: "#videoModal > div > div",
+  closeAboutUsModal: "#videoModal > div > div > div.modal-footer > button",
+  cartLink: "#navbarExample > ul > li:nth-child(4) > a",
+  loginLink: "#login2",
+  loginModal: "#logInModal > div > div",
+  closeLoginModal:
+    "#logInModal > div > div > div.modal-footer > button.btn.btn-secondary",
+  signupLink: "#signin2",
+  signupModal: "#signInModal > div > div",
+  closeSignupModal:
+    "#signInModal > div > div > div.modal-footer > button.btn.btn-secondary",
+};
